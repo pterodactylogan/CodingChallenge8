b676a84a80edcf2aa9281b353c26e419bb23cacc
ezikmundfisher <ezikmundfisher@smith.edu>
2018-04-19 19:50:36 -0400

    filtered out stopwords and put to lowercase

--- file_reading.jl
+++ file_reading.jl
@@ -18,7 +18,7 @@ for word in words
         numSentences=numSentences+1
     end
     #strip punctuation off of word and add it to dictionary
-    sword=strip(word, ['.','!','?'])
+    sword=lowercase(strip(word, ['.','!','?']))
     if(haskey(wordcounts, sword))
         wordcounts[sword]=wordcounts[sword]+1
     else
@@ -28,11 +28,22 @@ end
 
 #sort the counts by the values (high to low)
 orderedcounts=sort(collect(wordcounts), by = tuple -> last(tuple), rev=true)
-
+stopwords = ("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
+topten = []
+i = 1
+while length(topten) < 10
+    if !(orderedcounts[i][1] in stopwords)
+        push!(topten, orderedcounts[i][1])
+        i+=1
+    else
+        i+=1
+    end
+end
 #print results
 println(numWords, " words")
 println(numSentences, " sentences")
 for i in 1:10
-    println(orderedcounts[i][1], " ", orderedcounts[i][2])
+    println(topten[i])
 end
+
 close(f)
