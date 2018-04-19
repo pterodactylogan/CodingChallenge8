8a70dbc4d2b590fe68174f6d3510d9b67b8b4a28
pterodactylogan <loganswan37@gmail.com>
2018-04-19 19:59:54 -0400

    quick structural fix

--- file_reading.jl
+++ file_reading.jl
@@ -1,5 +1,5 @@
 #CHANGE PATH HERE
-path ="man_from_mars.txt"
+path ="/Users/Logan/Desktop/Logan/Smith/Junior Year/Advanced Programming/Coding Challenge VIII/man_from_mars.txt"
 #open file and read contents into array of words
 f=open(path)
 stuff = readstring(f)
@@ -10,6 +10,8 @@ numWords=0
 numSentences=0
 wordcounts=Dict() #keep track of counts of each words
 
+stopwords = ("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
+
 #loop through each word in the book
 for word in words
     numWords=numWords+1
@@ -18,7 +20,10 @@ for word in words
         numSentences=numSentences+1
     end
     #strip punctuation off of word and add it to dictionary
-    sword=lowercase(strip(word, ['.','!','?']))
+    sword=lowercase(strip(word, ['.','!','?','‘','\"']))
+    if(sword in stopwords)
+        continue
+    end
     if(haskey(wordcounts, sword))
         wordcounts[sword]=wordcounts[sword]+1
     else
@@ -28,22 +33,11 @@ end
 
 #sort the counts by the values (high to low)
 orderedcounts=sort(collect(wordcounts), by = tuple -> last(tuple), rev=true)
-stopwords = ("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
-topten = []
-i = 1
-while length(topten) < 10
-    if !(orderedcounts[i][1] in stopwords)
-        push!(topten, orderedcounts[i][1])
-        i+=1
-    else
-        i+=1
-    end
-end
 #print results
 println(numWords, " words")
 println(numSentences, " sentences")
 for i in 1:10
-    println(topten[i])
+   println(orderedcounts[i][1], " ", orderedcounts[i][2])
 end
 
 close(f)
